2e04e27d74d04d9c00e0033b82dc0eacac6f7440
soufianeBenRahim <32791470+soufianeBenRahim@users.noreply.github.com>
2018-05-12 11:49:23 +0100

    changeùment sur la tail de observtion et description 500 cahr

---
+++ Mini-ESante/src/main/java/com/xpertsoft/annuaireminesante/IAnnuair.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xpertsoft.annuaireminesante;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+
+
+/**
+ *
+ * @author Soufiane
+ */
+public interface IAnnuair extends Remote{
+     
+    public String Login(String Psudo,String Pass) throws RemoteException;
+    public String createAccont(String Psudo,String Pass) throws RemoteException;
+    public void DelletAccont(String Psudo)throws RemoteException;
+    public String GetIPUser(String Psudo) throws RemoteException;
+}
